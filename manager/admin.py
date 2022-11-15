7352e615c6dace28731ab37ea9a7a8f92af88922
Hesam Norin <hesam.norin@yahoo.com>
2022-11-15 22:40:19 +0330

    A few small changes, developed custom admin page

--- manager/admin.py
+++ manager/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from datetime import datetime
 from manager.models import App, Container
 
 @admin.register(App)
@@ -9,6 +10,16 @@ class ShopAdmin(admin.ModelAdmin):
 
 @admin.register(Container)
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ('name', 'app', 'created_at', 'last_run')
+    list_display = ('name', 'app', 'get_human_friendly_created_at', 'get_human_friendly_last_run')
     search_fields = ('name', 'status')
     list_filter = ('status',)
+
+    @admin.display(description='Created at')
+    def get_human_friendly_created_at(self, obj):
+        '''Returns human friendly created_at'''
+        return obj.created_at.strftime('%I:%M%p %d%b%Y')
+    
+    @admin.display(description='Last run')
+    def get_human_friendly_last_run(self, obj):
+        '''Returns human friendly last_run'''
+        return obj.last_run.strftime('%I:%M:%S%p %d%b%Y')
