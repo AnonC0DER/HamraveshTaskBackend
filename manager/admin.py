8748fe622c02cd4f9286f54b96a865d3c9d105ab
Hesam Norin <hesam.norin@yahoo.com>
2022-11-15 22:18:58 +0330

    Models created

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from manager.models import App, Container
 
-# Register your models here.
+@admin.register(App)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'created_at')
+    search_fields = ('name', 'user__username')
+
+
+@admin.register(Container)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'app', 'created_at', 'last_run')
+    search_fields = ('name', 'status')
+    list_filter = ('status',)
--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django_jalali.db import models as jmodels
 
-# Create your models here.
+class App(models.Model):
+    '''
+        App(s) model, contains main stuff, such as image, command, etc.
+    '''
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    image = models.CharField(max_length=260)
+    envs = models.JSONField(null=True, blank=True)
+    command = models.CharField(max_length=360)
+    created_at = jmodels.jDateTimeField('created at', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Container(models.Model):
+    '''
+        Container(s) model, connects to an app.
+        Each app can have multiple containers.
+    '''
+    class StatusChoices(models.TextChoices):
+        RUNNING = 'R', 'Running'
+        FINISHED = 'F', 'Finished'
+
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.FINISHED)
+    created_at = jmodels.jDateTimeField('created at', auto_now_add=True)
+    last_run = jmodels.jDateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
