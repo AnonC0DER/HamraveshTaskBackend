147885e6163788aedc709ab1cfdbad1d15d2996c
Hesam Norin <hesam.norin@yahoo.com>
2022-11-16 11:59:31 +0330

    Update serializers.py

--- manager/serializers.py
+++ manager/serializers.py
@@ -8,14 +8,6 @@ class ContainerSerializer(serializers.ModelSerializer):
 
 
 class AppSerializer(serializers.ModelSerializer):
-    containers = serializers.SerializerMethodField()
-
     class Meta:
         model = App
         exclude = ['user']
-
-    def get_containers(self, app):
-        '''Returns all containers'''
-        containers = app.container_set.all()
-        serializer = ContainerSerializer(containers, many=True)
-        return serializer.data
